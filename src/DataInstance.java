4890709248806302c761b46c8b76326bef73610c
GarethMcFarlane <gmcf8558@uni.sydney.edu.au>
2015-04-19 13:47:33 +1000

    Started on the DataInstance class and KNN.

---
+++ src/DataInstance.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Gareth McFarlane on 19/04/2015.
+ */
+public class DataInstance {
+List<DataRow> instanceList;
+String atrributeNames[];
+
+//Class for each row in the CSV file.
+    public class DataRow {
+        double attributes[];
+        String name; //Row's class.
+
+        //Constructor for a DataRow.  Takes a single input string and converts it into attributes.
+        public DataRow(String line) {
+            //Splits string into comma-delimited array.
+            String[] tokens = line.split(",",-1);
+            int dataSize = tokens.length;
+            attributes = new double[dataSize];
+
+            //Gets all attributes than finds the class name.
+            for (int i = 0; i < dataSize - 2; ++i) {
+                attributes[i] = Double.parseDouble(tokens[i]);
+                name = tokens[dataSize-1];
+            }
+        }
+    }
+
+    public DataInstance(String filename) {
+        instanceList = new ArrayList<DataRow>();
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                instanceList.add(new DataRow(line));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public DataRow pop(int index) {
+        return instanceList.remove(index);
+    }
+
+
+}
+
+
---
+++ src/kNearestNeighbour.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Gareth McFarlane on 19/04/2015.
+ */
+public class kNearestNeighbour {
+DataInstance data;
+int k;
+    public kNearestNeighbour(String filename, int k) {
+        data = new DataInstance(filename);
+        this.k = k;
+    }
+
+
+}
+
+
+
+
+
+
+
